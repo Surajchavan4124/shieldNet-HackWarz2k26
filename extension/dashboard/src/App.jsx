5be9b2eaf48f0392eb284826f8a7780bae694319
mewo-sunny <mckenziepereira1234@gmail.com>
2026-03-12 15:06:17 +0530

    extensions

--- extension/dashboard/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-900 text-white text-3xl">
-      ShieldNet Dashboard
-    </div>
-  );
-}
